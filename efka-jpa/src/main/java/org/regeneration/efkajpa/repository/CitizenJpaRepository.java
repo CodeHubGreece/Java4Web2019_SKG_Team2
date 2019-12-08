1e05bc76d52d10ef50c8c204d9a8f17a3d000a23
Alex <zaxas.alex@gmail.com>
2019-12-08 11:03:24 +0200

    "Entities changes"

--- efka-jpa/src/main/java/org/regeneration/efkajpa/repository/CitizenJpaRepository.java
+++ efka-jpa/src/main/java/org/regeneration/efkajpa/repository/CitizenJpaRepository.java
@@ -3,5 +3,5 @@ package org.regeneration.efkajpa.repository;
 import org.regeneration.efkajpa.entity.CitizenJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CitizenJpaRepository extends JpaRepository<CitizenJpa, String> {
+public interface CitizenJpaRepository extends JpaRepository<CitizenJpa, Long> {
 }
