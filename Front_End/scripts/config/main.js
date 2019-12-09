890e9a9e03c8f4f5f99566350212cbf09b5d4879
lasseris <milakis.c@gmail.comgit config --global user.name Christos>
2019-12-09 19:44:02 +0200

    Initial Commit

--- Front_End/scripts/config/main.js
+++ Front_End/scripts/config/main.js
@@ -1,2 +1,2 @@
 const LOCAL_STORAGE_LOGIN_TOKEN_NAME = "userid";
-const ROOT_PATH = "/home/geotzinos/project"
+const ROOT_PATH = "C:\Users/ReHome/Java4Web2019_SKG_Team2/Front_End"
--- Front_End/scripts/guest/authentication.js
+++ Front_End/scripts/guest/authentication.js
@@ -3,7 +3,7 @@ function login(usernameElement, passwordElement) {
     let username = usernameElement && usernameElement.value ? usernameElement.value : "";
     let password = passwordElement && passwordElement.value ? passwordElement.value : "";
 
-    if (username == "user" && password == "user") {
+    if (username === "user" && password === "user") {
         localStorage.setItem(LOCAL_STORAGE_LOGIN_TOKEN_NAME, true);
         window.location.replace(ROOT_PATH + "/pages/user/index.html");
     }
