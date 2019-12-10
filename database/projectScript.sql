3c558fe148ac0da840d04059f506475a357d54b0
AlexSKG <zaxas.alex@gmail.com>
2019-12-10 13:00:47 +0200

    database type changes

--- database/projectScript.sql
+++ database/projectScript.sql
@@ -1,57 +1,57 @@
 CREATE DATABASE EFKA CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE EFKA;
 
-DROP TABLE IF EXISTS appointment;
+DROP TABLE IF EXISTS appointments;
+DROP TABLE IF EXISTS citizens;
+DROP TABLE IF EXISTS doctors;
 DROP TABLE IF EXISTS specialties;
-DROP TABLE IF EXISTS citizen;
-DROP TABLE IF EXISTS doctor;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-id int auto_increment,
+id bigint auto_increment,
 username varchar(50) NOT NULL,
 `password` varchar(50) NOT NULL,
-`type` varchar(1) NOT NULL,
+`type` char(1) NOT NULL,
 PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE citizen (
-AMKA varchar(11) NOT NULL,
+CREATE TABLE citizens (
+amka varchar(11) NOT NULL,
 last_name varchar(50) NOT NULL,
 first_name varchar(50) NOT NULL,
 phone varchar(10) NOT NULL,
 email varchar(50) NOT NULL,
-user_id int,
+user_id bigint NOT NULL UNIQUE,
 PRIMARY KEY (AMKA),
 FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE specialties (
-specialty_id int auto_increment,
-specialty varchar(50),
+specialty_id int auto_increment NOT NULL,
+specialty varchar(50) NOT NULL,
 PRIMARY KEY (specialty_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE doctor (
+CREATE TABLE doctors (
 doctor_id varchar(5) NOT NULL,
 last_name varchar(50) NOT NULL,
 first_name varchar(50) NOT NULL,
-specialty int,
-user_id int,
+specialty_id int NOT NULL,
+user_id bigint NOT NULL UNIQUE,
 PRIMARY KEY (doctor_id),
 FOREIGN KEY (user_id) REFERENCES users (id),
-FOREIGN KEY (specialty) REFERENCES specialties (specialty_id)
+FOREIGN KEY (specialty_id) REFERENCES specialties (specialty_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE appointment (
-appointmentID int auto_increment, #gia na kanei monadiko arithmo apo mono tou #
-AMKA_C varchar(11) NOT NULL,
+CREATE TABLE appointments (
+appointment_id bigint auto_increment,
+amka_c varchar(11) NOT NULL,
 doctor_id varchar(5) NOT NULL,
 `time` time NOT NULL,
 `date` DATE  NOT NULL,
 illness_description varchar(50) NOT NULL,
 comments varchar(200) NOT NULL,
-PRIMARY KEY (appointmentID),
-FOREIGN KEY (AMKA_C) REFERENCES citizen (AMKA),
-FOREIGN KEY (doctor_id) REFERENCES doctor (doctor_id)
+PRIMARY KEY (appointment_id),
+FOREIGN KEY (amka_c) REFERENCES citizens (amka),
+FOREIGN KEY (doctor_id) REFERENCES doctors (doctor_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
