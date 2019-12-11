5804c97f3d231cf48e862371a29efd833a9a7b67
Posion <kyriazopoulosdim@gmail.com>
2019-12-11 18:30:05 +0200

    Update-UserController

--- efka-jpa/src/main/java/org/regeneration/efkajpa/service/DoctorService.java
+++ efka-jpa/src/main/java/org/regeneration/efkajpa/service/DoctorService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DoctorService {
     public Doctors retrieve (String id){
-        return new Doctors()
+        return new Doctors();
     }
 
 }
