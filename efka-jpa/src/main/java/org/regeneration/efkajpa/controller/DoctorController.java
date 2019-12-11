1fbef3707cefb18ad4fe84fc049d5f4eb5dc103d
AlexSKG <zaxas.alex@gmail.com>
2019-12-11 17:56:11 +0200

    Services and controllers

--- efka-jpa/src/main/java/org/regeneration/efkajpa/controller/DoctorController.java
+++ efka-jpa/src/main/java/org/regeneration/efkajpa/controller/DoctorController.java
@@ -1,48 +1,48 @@
-package org.regeneration.efkajpa.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.regeneration.efkajpa.entity.Doctors;
-import org.regeneration.efkajpa.repository.DoctorRepository;
-
-@RestController
-public class DoctorController {
-
-	@Autowired
-	private DoctorRepository doctorRepository;
-	
-	@GetMapping("/doctors/get/{id}")
-	public Doctor getDoctorById(@PathVariable("id") int id) {
-		return doctorRepository.retrieve(id);
-	}
-	
-	@GetMapping("/doctors/getAll")
-	//Returning is List is supported with JSON response
-	public List<Doctors> getAllDoctors() {
-		return doctorRepository.getAll();
-	}
-
-	@PostMapping("/doctor/create")
-	public Doctors createDoctor(@RequestBody Doctors doctor) {
-		doctorRepository.store(doctor);
-		return doctor;
-	}
-	
-	@GetMapping("/doctor/search/{name}")
-	public Doctors getDoctorByName(@PathVariable("name") String name) {
-		return doctorRepository.search(name);
-	}
-	
-	@DeleteMapping("/doctor/delete/{id}")
-	public Doctors deleteDoctorById(@PathVariable("id") int id) {
-		return doctorRepository.delete(id);
-	}
-}
+//package org.regeneration.efkajpa.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import org.regeneration.efkajpa.entity.Doctors;
+//import org.regeneration.efkajpa.repository.DoctorRepository;
+//
+//@RestController
+//public class DoctorController {
+//
+//	@Autowired
+//	private DoctorRepository doctorRepository;
+//
+//	@GetMapping("/doctors/get/{id}")
+//	public Doctors getDoctorByUserId(@PathVariable("id") Long id) {
+//		return doctorRepository.retrieve(id);
+//	}
+//
+//	/*@GetMapping("/doctors/getAll")
+//	//Returning is List is supported with JSON response
+//	public List<Doctors> getAllDoctors() {
+//		return doctorRepository.getAll();
+//	}
+//
+//	@PostMapping("/doctor/create")
+//	public Doctors createDoctor(@RequestBody Doctors doctors) {
+//		doctorRepository.store(doctors);
+//		return doctors;
+//	}
+//
+//	@GetMapping("/doctor/search/{name}")
+//	public Doctors getDoctorByName(@PathVariable("name") String name) {
+//		return doctorRepository.search(name);
+//	}
+//
+//	@DeleteMapping("/doctor/delete/{id}")
+//	public Doctors deleteDoctorById(@PathVariable("id") int id) {
+//		return doctorRepository.delete(id);*/
+//	}
+//}
---
+++ efka-jpa/src/main/java/org/regeneration/efkajpa/security/ApiUserDetailsService.java
@@ -0,0 +1,32 @@
+package org.regeneration.efkajpa.security;
+
+import org.regeneration.efkajpa.entity.Users;
+import org.regeneration.efkajpa.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Service
+public class ApiUserDetailsService implements UserDetailsService {
+
+    private UsersRepository usersRepository;
+
+    @Autowired
+    public void setUserRepository(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // find user in repository
+        Users user = usersRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        ApiUserDetails userDetails = new ApiUserDetails(user.getUsername(), user.getPassword(), user.getId(), user.getType());
+        return userDetails;
+    }
+}
