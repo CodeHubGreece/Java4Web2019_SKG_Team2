4c31984390b3a0125cf1c0fe0b2f076b75e5e161
DimitrisKyriazo <58390420+DimitrisKyriazo@users.noreply.github.com>
2019-12-12 17:09:43 +0200

    Delete CitizenRepository.java

--- efka-jpa/src/main/java/org/regeneration/efkajpa/repository/CitizenRepository.java
+++
@@ -1,7 +0,0 @@
-package org.regeneration.efkajpa.repository;
-
-import org.regeneration.efkajpa.entity.Citizens;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CitizenRepository extends JpaRepository<Citizens, String> {
-}
