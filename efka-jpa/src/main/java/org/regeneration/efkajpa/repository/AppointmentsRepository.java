5a625120fb0d9e35ecb50e1a483249e113b5054a
DimitrisKyriazo <58390420+DimitrisKyriazo@users.noreply.github.com>
2019-12-12 17:09:35 +0200

    Delete AppointmentsRepository.java

--- efka-jpa/src/main/java/org/regeneration/efkajpa/repository/AppointmentsRepository.java
+++
@@ -1,7 +0,0 @@
-package org.regeneration.efkajpa.repository;
-
-import org.regeneration.efkajpa.entity.Appointments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentsRepository extends JpaRepository<Appointments, Long> {
-}
