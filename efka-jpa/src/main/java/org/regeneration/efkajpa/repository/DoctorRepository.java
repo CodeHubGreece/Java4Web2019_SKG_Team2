25fbf6c76882349694daa04745db3d4c579ffc8c
DimitrisKyriazo <58390420+DimitrisKyriazo@users.noreply.github.com>
2019-12-12 17:10:01 +0200

    Delete DoctorRepository.java

--- efka-jpa/src/main/java/org/regeneration/efkajpa/repository/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.regeneration.efkajpa.repository;
-
-import org.regeneration.efkajpa.entity.Doctors;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctors, String> {
-}
