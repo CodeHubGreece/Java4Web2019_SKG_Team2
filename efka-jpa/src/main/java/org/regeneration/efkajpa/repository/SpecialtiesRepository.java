ce46e671ac517666df3589e5d8ad82efd876d535
DimitrisKyriazo <58390420+DimitrisKyriazo@users.noreply.github.com>
2019-12-12 17:10:11 +0200

    Delete SpecialtiesRepository.java

--- efka-jpa/src/main/java/org/regeneration/efkajpa/repository/SpecialtiesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.regeneration.efkajpa.repository;
-
-import org.regeneration.efkajpa.entity.Specialties;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpecialtiesRepository extends JpaRepository<Specialties, Integer> {
-}
