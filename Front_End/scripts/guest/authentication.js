e4e20fa06e1974af135cf278a32c497e8976a378
Ioanna <ibah2005@yahoo.gr>
2019-12-15 14:35:31 +0200

    New

--- Front_End/scripts/guest/authentication.js
+++ Front_End/scripts/guest/authentication.js
@@ -1,26 +1,79 @@
 
 function login(usernameElement, passwordElement) {
-    let username = usernameElement && usernameElement.value ? usernameElement.value : "";
-    let password = passwordElement && passwordElement.value ? passwordElement.value : "";
-    let type = document.getElementById("userType").value;
+     let username = usernameElement && usernameElement.value ? usernameElement.value : "";
+     let password = passwordElement && passwordElement.value ? passwordElement.value : "";
+     let type = document.getElementById("userType").value;
 
-    if (username === "user" && password === "user") {
-        localStorage.setItem(LOCAL_STORAGE_LOGIN_TOKEN_NAME, true);
-        window.location.replace(ROOT_PATH + "/pages/user/index.html");
+ 
+
+const ROOT_PATH = "http://localhost:8080";
+var fd = new FormData();
+fd.append("username", username);
+fd.append("password", password);
+$.ajax({
+  url: ROOT_PATH + "/login",
+  data: fd,
+  processData: false,
+  contentType: false,
+  type: "POST",
+  success: function(data) {
+    sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
+
+    $.ajax({
+      let type = document.getElementById("userType").value,
+      url: ROOT_PATH + "/user",
+      data: fd,
+      processData: false,
+      contentType: false,
+      type: "GET",
+      success: function(data){    
+        if(type=="doctor"){
+          window.location.replace(ROOT_PATH + "/pages/user/doctor/doctorSearch.html");
+        }else if(type=="citizen"){
+          window.location.replace(ROOT_PATH + "/pages/user/citizen/createPage.html");
+        }
+
+      window.location.replace(ROOT_PATH + "/pages/user/citizen/createPage.html");
+  },
+  statusCode: {
+    401: function() {
+      alert("Invalid username or password!");
     }
-    else {
-        alert("Wrong password");
-    }
+  }
+});
+}
 }
 
 function register() {
-    // TODO:
+  let amka = document.getElementById('amka').value;
+  let name = document.getElementById('name').value;
+  let lastname = document.getElementById('lastname').value;
+  let email = document.getElementById('email').value;
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let phone = document.getElementById('phone').value;
+             $.ajax({
+                       url: ROOT_PATH + '/guest/register',
+                       type: 'POST',
+                       dataType: 'json',
+                       data: JSON.stringify({
+                       amka:amka,
+                       name:name,
+                       lastname:lastname,
+                       email:email,
+                       username:username,
+                       password:password,                      
+                       phone:phone,                          
+                  }),
+                  contentType: 'application/json',
+                  success: function (result) {
+                      alert("User Saved: " + JSON.stringify(result));
+                  },
+                  error: function (xhr, resp, text) {
+                      alert("User not Saved: " + text);
+                  }
+              });
 }
+	
+    
 
-
-
-	if(type==="doctor"){
-		location.replace("./user/doctor/search.html");
-	} else if(type==="citizen"){
-		location.replace("./user/citizen/createPage.html");
-	}
